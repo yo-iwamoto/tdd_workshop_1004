bd30e122d0521424898d91a78b738d0ff27607db
you-5805 <you.iwamoto@prtimes.co.jp>
2022-10-04 12:19:44 +0900

    add c8 coverage

--- src/FizzBuzz.spec.ts
+++ src/FizzBuzz.spec.ts
@@ -1,24 +1,40 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { FizzBuzz } from './FizzBuzz';
 
 const fizzBuzz = new FizzBuzz();
 
 describe('数値変換を行うFizzBuzz', () => {
   describe('3の倍数の時Fizzに変換する', () => {
-    test('3を渡すとFizzを返す', () => {
-      expect(fizzBuzz.convert(3), 'Fizz');
+    it('3を渡すとFizzを返す', () => {
+      expect(fizzBuzz.convert(3)).toBe('Fizz');
     });
   });
 
   describe('5の倍数の時Buzzに変換する', () => {
-    test('5を渡すとBuzzを返す', () => {
-      expect(fizzBuzz.convert(5), 'Buzz');
+    it('5を渡すとBuzzを返す', () => {
+      expect(fizzBuzz.convert(5)).toBe('Buzz');
+    });
+  });
+
+  describe('3と5の公倍数の時FizzBuzzに変換する', () => {
+    it('15を渡すとFizzBuzzを返す', () => {
+      expect(fizzBuzz.convert(15)).toBe('FizzBuzz');
     });
   });
 
   describe('その他の数値はそのまま文字列に変換する', () => {
-    test('1を渡すと文字列の1を返す', () => {
-      expect(fizzBuzz.convert(1), '1');
+    it('1を渡すと文字列の1を返す', () => {
+      expect(fizzBuzz.convert(1)).toBe('1');
+    });
+  });
+
+  describe('境界値外の入力であっても、同様の変換規則で動作する', () => {
+    it('0を渡すとFizzBuzzを返す', () => {
+      expect(fizzBuzz.convert(0)).toBe('FizzBuzz');
+    });
+
+    it('101を渡すと文字列101を返す', () => {
+      expect(fizzBuzz.convert(101)).toBe('101');
     });
   });
 });
--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,13 +1,23 @@
 export class FizzBuzz {
+  /**
+   * @param num 自然数
+   *
+   * @returns 3の倍数のときFizz、5の倍数のときBuzz、3と5の倍数のときFizzBuzz、それ以外のとき文字列
+   */
   convert(num: number) {
+    const words: string[] = [];
+
     if (num % 3 === 0) {
-      return 'Fizz';
+      words.push('Fizz');
     }
-
     if (num % 5 === 0) {
-      return 'Buzz';
+      words.push('Buzz');
     }
 
-    return num;
+    if (words.length === 0) {
+      return num.toString();
+    }
+
+    return words.join('');
   }
 }

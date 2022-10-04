d659d3d59d14a4df864ae85cb1c50606a64f7a47
you-5805 <you.iwamoto@prtimes.co.jp>
2022-10-04 16:11:40 +0900

    refactor, add test case by test.each

--- src/ClosedRange.spec.ts
+++ src/ClosedRange.spec.ts
@@ -15,46 +15,76 @@ describe('閉区間を表現する ClosedRange クラスの実装', () => {
       expect(initInvalidRange).not.toThrowError();
     });
 
-    test('下端点 1 に対して境界値外である 0 を渡すと例外を投げない', () => {
+    test('下端点 1 に対して境界値外である 0 を渡すと例外を投げる', () => {
       const initInvalidRange = () => new ClosedRange(2, 1);
       expect(initInvalidRange).toThrowError();
     });
   });
 
   describe('任意の整数が閉区間に含まれるか判定できる', () => {
-    test('[1,1] のインスタンスで includes に 1 を渡すと true を返す', () => {
-      const r = new ClosedRange(1, 1);
-      expect(r.includes(1)).toBe(true);
+    test('[1,3] の ClosedRange で includes に 0 を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 3);
+      expect(r.includes(0)).toBe(false);
     });
 
-    test('[1,1] のインスタンスで 0 を渡すと false を返す', () => {
-      const r = new ClosedRange(1, 1);
-      expect(r.includes(0)).toBe(false);
+    test.each<number>([1, 2, 3])(
+      '[1,3] の ClosedRange で includes に %d を渡すと true を返す',
+      (n) => {
+        const r = new ClosedRange(1, 3);
+        expect(r.includes(n)).toBe(true);
+      },
+    );
+
+    test('[1,3] の ClosedRange で includes に 4 を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 3);
+      expect(r.includes(4)).toBe(false);
     });
   });
 
-  describe('別の閉区間との関係を判定できる', () => {
-    test('[1,1] のインスタンスで isEqualTo に [1,1] を渡すと true を返す', () => {
-      const r = new ClosedRange(1, 1);
-      const another = new ClosedRange(1, 1);
+  describe('別の閉区間と一致することを isEaualTo で判定できる', () => {
+    test('[1,3] の ClosedRange で [1,3] を渡すと true を返す', () => {
+      const r = new ClosedRange(1, 3);
+      const another = new ClosedRange(1, 3);
       expect(r.isEqualTo(another)).toBe(true);
     });
 
-    test('[1,1] のインスタンスで isEqualTo に [1,2] を渡すと false を返す', () => {
-      const r = new ClosedRange(1, 1);
-      const another = new ClosedRange(1, 2);
+    test('[1,3] の ClosedRange で [1,4] を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 3);
+      const another = new ClosedRange(1, 4);
       expect(r.isEqualTo(another)).toBe(false);
     });
 
-    test('[1,1] のインスタンスで contains に [1,1] を渡すと true を返す', () => {
-      const r = new ClosedRange(1, 5);
-      const another = new ClosedRange(2, 4);
-      expect(r.contains(another)).toBe(true);
+    test('[1,3] の ClosedRange で [0,3] を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 3);
+      const another = new ClosedRange(0, 3);
+      expect(r.isEqualTo(another)).toBe(false);
+    });
+  });
+
+  describe('別の閉区間を包含することを contains で判定できる', () => {
+    describe.each<number>([1, 2, 3])('[1,5] の ClosedRange に 下端 %d で', (n) => {
+      test.each<number>([3, 4, 5])('上端 %d の ClosedRange を渡すと true を返す', (m) => {
+        const r = new ClosedRange(1, 5);
+        const another = new ClosedRange(n, m);
+        expect(r.contains(another)).toBe(true);
+      });
     });
 
-    test('[1,1] のインスタンスで contains に [1,2] を渡すと false を返す', () => {
+    test('[1,5] の ClosedRange に両端が範囲外の [0,6] を渡すと false を返す', () => {
       const r = new ClosedRange(1, 5);
       const another = new ClosedRange(0, 6);
+      expect(r.contains(another)).toBe(false);
+    });
+
+    test('[1,5] の ClosedRange に下端だけが範囲外の [0,5] を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 5);
+      const another = new ClosedRange(0, 5);
+      expect(r.contains(another)).toBe(false);
+    });
+
+    test('[1,1] の ClosedRange に上端が範囲外の [1,6] を渡すと false を返す', () => {
+      const r = new ClosedRange(1, 5);
+      const another = new ClosedRange(1, 6);
       expect(r.contains(another)).toBe(false);
     });
   });
